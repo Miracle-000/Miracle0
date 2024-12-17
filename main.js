970d9228b5109f56b384bfdca09e7fe502150bdb
Miracle-000 <elenalus288@gmail.com>
2024-12-17 23:31:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+iconsoc = document.getElementById('square')
+const body = document.body
+const gallery = document.getElementById('gallery')
+let square = document.getElementById('square')
+let logo= document.getElementsByClassName('logo')
+let test = document.getElementsByClassName("test")
+let vid = document.getElementById("video")
+let noise = document.getElementById("noise")
+let scrolll = document.getElementById("scrolll")
+let soc = document.getElementsByClassName('socs')
+const aphish = document.querySelector('.aphish')
+let cbs = document.querySelectorAll('.cbimg')
+
+let mute=0
+
+
+
+let bg = new Image()
+bg.src="/media/bg.avif"
+
+bg.onload =()=> {
+    document.body.style.background='url("/media/bg.avif")'
+}
+
+
+let aph = new Image()
+aph.src = '/media/aphish.avif'
+
+aph.onload = ()=>{
+    document.querySelector('.aphish').src=aph.src
+    document.querySelector('.aphish').classList.add('aphishafter')
+    console.log(document.querySelector('.aphish'))
+}
+
+
+
+
+
+
+
+
+prog = document.getElementsByClassName('prog')
+const fullimg = new Image()
+fullimg.src= aphish.getAttribute('data-src')
+fullimg.onload = ()=>{
+    aphish.src=fullimg.src
+    aphish.classList.add('loaded')
+}
+
+
+
+vid.addEventListener('click', muted)
+
+
+console.log(cbs)
+let imgobs = new IntersectionObserver((entries,observer)=>{
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            let img = entry.target
+            img.src = img.dataset.src
+            img.classList.add('aftersee')
+            observer.unobserve(img)}})},{})
+
+
+cbs.forEach(it=>{
+    imgobs.observe(it)
+})
+
+
+
+
+function muted(){
+    if ( mute==0) {
+        vid.muted=""
+        noise.style.backgroundImage='url("media/noize.png")'
+        mute=1
+    }
+    else {
+        vid.muted="muted"
+        noise.style.backgroundImage='url("media/nonoize.png")'
+        mute=0
+    }
+}
